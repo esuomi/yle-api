9340532379e5bcdec836aa0100a1ab5a8bd85892
Esko Suomi <suomi.esko@gmail.com>
2015-05-17 13:12:35 +0300

    base injections for testing

--- src/test/java/io/induct/yle/YleApiTestingBase.java
+++ src/test/java/io/induct/yle/YleApiTestingBase.java
@@ -2,8 +2,12 @@ package io.induct.yle;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import io.induct.daniel.DanielModule;
 import io.induct.yle.ioc.YleApiModule;
 import org.junit.BeforeClass;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 
 /**
  * @since 2015-05-09
@@ -14,6 +18,12 @@ public abstract class YleApiTestingBase {
 
     @BeforeClass
     public static void preSetUp() throws Exception {
-        injector = Guice.createInjector(new YleApiModule());
+        injector = Guice.createInjector(
+                new DanielModule(),
+                new YleApiModule());
+    }
+
+    protected InputStream asStream(String s) {
+        return new ByteArrayInputStream(s.getBytes());
     }
 }
