b0dff20bc37a31795750b1174834be22783a59e8
Esko Suomi <suomi.esko@gmail.com>
2015-05-17 13:38:39 +0300

    move search related classes to their own sub-feature package

---
+++ src/main/java/io/induct/Example.java
@@ -0,0 +1,23 @@
+package io.induct;
+
+import io.induct.yle.api.YleApi;
+import io.induct.yle.api.YleId;
+import io.induct.yle.api.profiles.Feature;
+import io.induct.yle.api.profiles.YleProfilesApi;
+
+/**
+ * @since 2015-05-11
+ */
+public class Example {
+
+    public static void main(String[] args) {
+        YleApi yleApi = null;
+
+        YleProfilesApi profilesApi = yleApi.profiles();
+
+        YleId identity = new YleId("3-24234");
+
+        Feature feature = new Feature(identity);
+        profilesApi.save(feature);
+    }
+}
