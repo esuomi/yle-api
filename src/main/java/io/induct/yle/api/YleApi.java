4aa3c4ec37a1b045010667b0ed4b80c6b5456ff3
Esko Suomi <suomi.esko@gmail.com>
2015-09-09 16:18:58 +0300

    Implement ability to generate CDN URLs with support for Cloudinary's transforms.
    Note that Cloudinary's Transform class is not the best level of quality as Java code goes but it could be worse. Only core module is included as there is no need for actual Cloudinary API calling facilities.

--- src/main/java/io/induct/yle/api/YleApi.java
+++ src/main/java/io/induct/yle/api/YleApi.java
@@ -3,6 +3,7 @@ package io.induct.yle.api;
 import io.induct.yle.api.media.YleMediaApi;
 import io.induct.yle.api.programs.YleProgramsApi;
 import io.induct.yle.api.tracking.YleTrackingApi;
+import io.induct.yle.cdn.images.YleImagesCdn;
 
 import javax.inject.Inject;
 
@@ -14,12 +15,14 @@ public class YleApi {
     private final YleProgramsApi programsApi;
     private final YleMediaApi mediaApi;
     private final YleTrackingApi trackingApi;
+    private final YleImagesCdn imageCdn;
 
     @Inject
-    public YleApi(YleProgramsApi programsApi, YleMediaApi mediaApi, YleTrackingApi trackingApi) {
+    public YleApi(YleProgramsApi programsApi, YleMediaApi mediaApi, YleTrackingApi trackingApi, YleImagesCdn imageCdn) {
         this.programsApi = programsApi;
         this.mediaApi = mediaApi;
         this.trackingApi = trackingApi;
+        this.imageCdn = imageCdn;
     }
 
     public YleProgramsApi programs() {
@@ -32,5 +35,9 @@ public class YleApi {
 
     public YleTrackingApi tracking() {
         return trackingApi;
+    }
+
+    public YleImagesCdn imagesCdn() {
+        return imageCdn;
     }
 }
--- src/test/java/io/induct/yle/TestDependenciesModule.java
+++ src/test/java/io/induct/yle/TestDependenciesModule.java
@@ -11,7 +11,8 @@ import io.induct.http.HttpClient;
  */
 public class TestDependenciesModule extends AbstractModule {
 
-    public static final String TESTING_BASE_URL = "http://yle.api";
+    public static final String TESTING_API_BASE_URL = "http://yle.api";
+    public static final String TESTING_CDN_BASE_URL = "http://yle.cdn";
     public static final String TESTING_RATE_LIMIT = "100";
     public static final String TESTING_APP_ID = "TEST_APP_ID";
     public static final String TESTING_APP_KEY = "TEST_APP_KEY";
@@ -27,7 +28,8 @@ public class TestDependenciesModule extends AbstractModule {
     protected void configure() {
         bind(HttpClient.class).toInstance(httpClient);
 
-        bind(String.class).annotatedWith(Names.named("yle.api.baseUrl")).toInstance(TESTING_BASE_URL);
+        bind(String.class).annotatedWith(Names.named("yle.images.baseUrl")).toInstance(TESTING_CDN_BASE_URL);
+        bind(String.class).annotatedWith(Names.named("yle.api.baseUrl")).toInstance(TESTING_API_BASE_URL);
         bind(String.class).annotatedWith(Names.named("yle.api.rateLimit")).toInstance(TESTING_RATE_LIMIT);
         bind(String.class).annotatedWith(Names.named("yle.api.appId")).toInstance(TESTING_APP_ID);
         bind(String.class).annotatedWith(Names.named("yle.api.appKey")).toInstance(TESTING_APP_KEY);
